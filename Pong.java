895ca7933a1dd3d2b1476a36d129aede23cf1c6a
Christian Marte <cmarte2493@gmail.com>
2020-06-07 17:19:10 -0400

    intial commit

---
+++ Pong.java
@@ -0,0 +1,134 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+
+
+import java.util.Random;
+import java.util.Timer;
+
+public class Pong extends Canvas
+{
+	Point delta;
+	Ellipse2D.Double ball;
+    Rectangle paddle1;
+    Rectangle paddle2;
+    int p1Score=0;
+    int p2Score=0;
+    
+	
+	public static void main( String[] args )
+	{
+		JFrame win = new JFrame("Pong");
+		win.setSize(1010,735);
+		win.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		win.add( new Pong() );
+		win.setVisible(true);
+	}
+
+	public Pong()
+	{
+		enableEvents(java.awt.AWTEvent.KEY_EVENT_MASK);
+		requestFocus();
+		
+		ball = new Ellipse2D.Double(500,350,20,20);
+		delta = new Point(-5,5);
+        paddle1 = new Rectangle(50,250,20,200);
+        paddle2 = new Rectangle(900,250,20,200);
+        
+		
+		Timer t = new Timer(true);
+		t.schedule( new java.util.TimerTask()
+		{
+			public void run()
+			{
+				gameRunner();
+				repaint();
+			}
+		}, 10, 10);
+
+	}
+
+	public void paint( Graphics g )
+	{
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.black);
+		g2.fill(ball);
+		
+		g2.setColor(Color.blue);
+        g2.fill(paddle1);
+        
+        g2.setColor(Color.red);
+        g2.fill(paddle2);
+        
+        g2.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+        g2.drawString("Player1 Score: "+ String.valueOf(p1Score), 100, 100);
+        g2.drawString("Player2 Score: "+ String.valueOf(p2Score), 700, 100);
+
+	}
+
+	public void processKeyEvent(KeyEvent e)
+	{
+		if ( e.getID() == KeyEvent.KEY_PRESSED )
+		{
+			if ( e.getKeyCode() == KeyEvent.VK_W )
+			{
+				paddle1.y -= 10; 
+			}
+			if ( e.getKeyCode() == KeyEvent.VK_S )
+			{
+				paddle1.y += 10;
+            }
+            if ( e.getKeyCode() == KeyEvent.VK_UP )
+			{
+				paddle2.y -= 10; 
+			}
+			if ( e.getKeyCode() == KeyEvent.VK_DOWN )
+			{
+				paddle2.y += 10;
+			}
+        }
+        
+	}
+	
+	public void gameRunner()
+	{
+		ball.x += delta.x;
+		ball.y += delta.y;
+
+		// Bounces if wall is hit
+		if ( ball.y < 0 || ball.y+20 > 700 )
+			delta.y = -delta.y;
+		
+		// Checks if ball is hitting the paddle
+		if ( ball.intersects(paddle1) )
+			delta.x = -delta.x;
+			
+        if ( ball.intersects(paddle2) )
+			delta.x = -delta.x;
+			
+            // Scoring check
+        
+        
+            if ( ball.x > 1000 )
+		{
+			ball.x = 500;
+            ball.y = 350;
+            p1Score++;
+            delta = new Point(-5,5);
+		}
+			
+        if ( ball.x < 0 )
+		{
+			ball.x = 500;
+            ball.y = 350;
+            p2Score++;
+            delta = new Point(-5,5);  
+		}
+			
+		
+	}
+	
+	public boolean isFocusable() { return true;	}
+}
